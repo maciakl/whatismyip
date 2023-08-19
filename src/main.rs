5801e342658cd3d14ed6c46084ac189c3ccb3ef9
Luke Maciak <luke@maciak.net>
2023-08-18 21:19:49 -0400

    Added option to show local ip

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,61 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None )]
+struct Args {
+    /// get local ip address instead of the remote one
+    #[arg(short, long)]
+    local: bool,
+}
+
 fn main() {
 
-     let resp = reqwest::blocking::get("https://api.ipify.org");
+    let args = Args::parse();
 
-     match resp {
+    if args.local {
+        show_local();
+    }
+    else {
+        show_ip();
+    }
+
+}
+
+
+fn show_ip() {
+
+    // Query the remote service to get the IP
+    let resp = reqwest::blocking::get("https://api.ipify.org");
+
+    match resp {
 
         Ok(r) => {
             let ip = r.text().unwrap();
             println!("{}", &ip);
-        }
-        
+        },
+
         Err(_) => {
             println!("network error");
         }
+    }
+}
 
-     }
+
+fn show_local() {
+    use local_ip_address::local_ip;
+
+    let resp = local_ip();
+
+    match resp {
+
+        Ok(ip) => {
+            println!("{}", ip);
+        },
+
+        Err(_) => {
+            println!("failed to get local ip");
+        }
+
+    }
 
 }
